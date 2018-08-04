725a3a3d78d85da99e9f0a595d6dc928ac4f4ce2
kwangin <inylove82@gmail.com>
2018-08-04 18:12:50 +0900

    Add jquery script(bootstrap requires it)

--- src/main/scala/akka/minion/HttpServer.scala
+++ src/main/scala/akka/minion/HttpServer.scala
@@ -127,6 +127,12 @@ object Template {
           href := "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
         ),
         script(
+          src := "https://code.jquery.com/jquery-3.3.1.min.js"
+        ),
+        script(
+          src := "https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
+        ),
+        script(
           src := "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
         ),
         link(
